38151008c3e891ce7d13f36061b360577de1f86e
karthi <karthikraj577@gmail.com>
2022-03-29 22:41:53 +0530

    data

--- fetch.js
+++ fetch.js
@@ -1,21 +1,26 @@
 var container = document.getElementById("content");
-var table = document.getElementById("tbody");
+var tableData = document.getElementById("tbody");
+var getData = document.getElementById("status");
 
 const fetchData = async () => {
   try {
     const res = await fetch("db.json");
     data = await res.json();
     console.log(data);
-    data.map(x => {
-      return (table.innerHTML += `
+    {
+      getData.addEventListener("click", function (e) {
+        data.map(x => {
+          return (tableData.innerHTML += `
        <tr>
-       <td>${x.capsule_id}</td>
+       <td >${x.capsule_id}</td>
        <td>${x.missions[0].flight}</td>
        <td>${x.details}</td>
        </tr>
       
         `);
-    });
+        });
+      });
+    }
   } catch (e) {
     console.log(e);
   }
